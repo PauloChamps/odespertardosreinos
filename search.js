53d37e9f4eb23782db9c12580d1615f87676a92b
Paulo Campos <121526199+PauloChamps@users.noreply.github.com>
2024-05-27 16:07:32 -0300

    Add files via upload

--- search.js
+++ search.js
@@ -1,34 +1,8 @@
-document.getElementById('search-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var searchTerm = document.getElementById('search-input').value.trim().toLowerCase();
-    console.log('Termo de busca:', searchTerm);
-    searchImages(searchTerm);
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var menuToggle = document.querySelector('.menu-toggle');
+    var menu = document.querySelector('#menu-h ul');
 
-function searchImages(searchTerm) {
-    console.log('Pesquisando imagens para:', searchTerm);
-    var searchResults = document.getElementById('search-results');
-    searchResults.innerHTML = ''; // Limpa os resultados anteriores
-
-    // Lista de nomes de arquivos de imagens (substitua com os seus nomes de arquivo)
-    var imageFiles = ['boss_arquidemonio.jpeg', 'imagem2.jpg', 'imagem3.jpg'];
-
-    // Filtra os nomes de arquivo que contêm o termo de busca
-    var filteredImages = imageFiles.filter(function(imageFile) {
-        return imageFile.toLowerCase().includes(searchTerm);
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('show');
     });
-
-    console.log('Imagens encontradas:', filteredImages);
-
-    // Exibe os resultados na página
-    if (filteredImages.length > 0) {
-        filteredImages.forEach(function(imageFile) {
-            var imgElement = document.createElement('img');
-            imgElement.src = 'arquivos/' + imageFile;
-            imgElement.alt = imageFile; // Use o nome do arquivo como texto alternativo
-            searchResults.appendChild(imgElement);
-        });
-    } else {
-        searchResults.textContent = 'Nenhuma imagem encontrada.';
-    }
-}
+});
