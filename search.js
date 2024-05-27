23d3b22962f35798a6bcafbeece285729c1117db
Paulo Campos <121526199+PauloChamps@users.noreply.github.com>
2024-05-27 17:21:28 -0300

    Add files via upload

--- search.js
+++ search.js
@@ -2,11 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     var menuToggle = document.querySelector('.menu-toggle');
     var menu = document.querySelector('#menu-h ul');
 
-    console.log('Menu toggle button:', menuToggle);
-    console.log('Menu:', menu);
-
+    // Adicionar um event listener para o clique no botão do menu
     menuToggle.addEventListener('click', function() {
         menu.classList.toggle('show');
-        console.log('Menu toggled, current class:', menu.className);
+    });
+
+    // Adicionar um event listener para o clique fora do menu
+    document.addEventListener('click', function(event) {
+        var isClickInsideMenu = menu.contains(event.target);
+        var isClickOnMenuToggle = menuToggle.contains(event.target);
+
+        if (!isClickInsideMenu && !isClickOnMenuToggle) {
+            menu.classList.remove('show');
+        }
     });
 });
+
+
+
