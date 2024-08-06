02ff81bc25bc5c0a4a3b07ca62d0406e86324928
Paulo Campos <121526199+PauloChamps@users.noreply.github.com>
2024-08-06 14:59:36 -0300

    Add files via upload

--- search.js
+++ search.js
@@ -77,3 +77,146 @@ function closeOverlay(event) {
     overlay.style.display = "none";
   }
 }
+
+
+
+//barra filtragem
+document.addEventListener('DOMContentLoaded', function() {
+    const filterSelect = document.getElementById('filter-select');
+    const itemsContainer = document.getElementById('items-container');
+
+    filterSelect.addEventListener('change', function() {
+        const selectedValue = this.value;
+        filterItems(selectedValue);
+    });
+
+
+    function filterItems(filterType) {
+        const items = document.querySelectorAll('.image-item-magic');
+        items.forEach(item => {
+            let shouldDisplay = true;
+
+            // Reset display
+            item.style.display = 'block';
+
+
+            switch (filterType) {
+                case 'nome-asc':
+                    sortItems('asc');
+                    shouldDisplay = false;
+                    break;
+                case 'nome-desc':
+                    sortItems('desc');
+                    shouldDisplay = false;
+                    break;
+                case 'tipo-uv':
+                    shouldDisplay = item.getAttribute('data-tipo') === 'U.V';
+                    break;
+                case 'tipo-mv':
+                    shouldDisplay = item.getAttribute('data-tipo') === 'M.V';
+                    break;
+                case 'carta-hibridos':
+                    shouldDisplay = item.getAttribute('data-carta') === 'Híbridos';
+                    break;
+                case 'carta-humanos':
+                    shouldDisplay = item.getAttribute('data-carta') === 'Humanos';
+                    break;
+                case 'carta-monstros':
+                    shouldDisplay = item.getAttribute('data-carta') === 'Monstros';
+                    break;
+                case 'carta-elementais':
+                    shouldDisplay = item.getAttribute('data-carta') === 'Elementais';
+                    break;
+                case 'carta-dano':
+                    shouldDisplay = item.getAttribute('data-carta2') === 'Dano';
+                    break;
+                case 'carta-vida':
+                    shouldDisplay = item.getAttribute('data-carta3') === 'Vida';
+                    break;
+                case 'carta-sorte':
+                    shouldDisplay = item.getAttribute('data-carta4') === 'Sorte';
+                    break;
+                case 'carta-bloqueio':
+                    shouldDisplay = item.getAttribute('data-carta5') === 'Bloqueio';
+                    break;
+                case 'carta-ressurreicao':
+                    shouldDisplay = item.getAttribute('data-carta6') === 'Ressurreição';
+                    break;
+                case 'carta-destruicao':
+                    shouldDisplay = item.getAttribute('data-carta7') === 'Destruição';
+                    break;
+                case 'carta-mundos':
+                    shouldDisplay = item.getAttribute('data-carta8') === 'Mundos';
+                    break;
+                case 'carta-gplus':
+                    shouldDisplay = item.getAttribute('data-carta9') === 'G+';
+                    break;
+                case 'carta-ex':
+                    shouldDisplay = item.getAttribute('data-carta10') === 'Ex';
+                    break;
+                default:
+                    shouldDisplay = true;
+                    break;
+            }
+
+            item.style.display = shouldDisplay ? 'block' : 'none';
+        });
+    }
+
+    function sortItems(order) {
+        const items = Array.from(document.querySelectorAll('.image-item-magic'));
+        items.sort((a, b) => {
+            const nameA = a.querySelector('h3').textContent.toUpperCase();
+            const nameB = b.querySelector('h3').textContent.toUpperCase();
+
+            if (order === 'asc') {
+                return nameA.localeCompare(nameB);
+            } else {
+                return nameB.localeCompare(nameA);
+            }
+        });
+
+        items.forEach(item => itemsContainer.appendChild(item));
+    }
+});
+
+
+
+
+
+// SLIDER PAGINA HOME
+
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.banner .slide');
+    const pointers = document.querySelectorAll('.banner .pointer');
+    let currentSlide = 0;
+
+    function goToSlide(slideIndex) {
+        document.querySelector('.banner .sliders').style.transform = `translateX(-${slideIndex * 100}vw)`;
+        pointers.forEach((pointer, index) => {
+            pointer.classList.toggle('active', index === slideIndex);
+        });
+        currentSlide = slideIndex;
+    }
+
+    function nextSlide() {
+        const nextIndex = (currentSlide + 1) % slides.length;
+        goToSlide(nextIndex);
+    }
+
+    function prevSlide() {
+        const prevIndex = (currentSlide - 1 + slides.length) % slides.length;
+        goToSlide(prevIndex);
+    }
+
+    pointers.forEach((pointer, index) => {
+        pointer.addEventListener('click', () => {
+            goToSlide(index);
+        });
+    });
+
+    setInterval(nextSlide, 7000);
+
+    goToSlide(0);
+});
+
